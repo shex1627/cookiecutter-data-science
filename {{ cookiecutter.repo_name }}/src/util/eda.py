13fb13ed46d8cfc2529fcbab41693a84b06742b8
Shicheng Huang <shicheng@symantec.com>
2019-05-16 10:32:48 -0700

    adding a function that showing missing value counts nad percentage

--- {{ cookiecutter.repo_name }}/src/util/eda.py
+++ {{ cookiecutter.repo_name }}/src/util/eda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def cdfpdf(df_col):
     """
@@ -14,3 +15,45 @@ def cdfpdf(df_col):
     pdf_df.columns = [df_col.name, 'pdf']
     pdf_df['cdf'] = np.cumsum(pdf_df['pdf'])
     return pdf_df
+
+
+def na_agg(df, cols=[], missing_only=False, plot=True):
+    if len(cols) == 0:
+        cols = df.columns
+    # Capture the necessary data
+    count = []
+
+    for variable in cols:
+        length = df[variable].count()
+        count.append(length)
+
+    count_pct = np.round(100 * pd.Series(count) / len(df), 2)
+    count = pd.Series(count)
+
+    missing = pd.DataFrame()
+    missing['variables'] = cols
+    missing['count'] = len(df) - count
+    missing['count_pct'] = 100 - count_pct
+
+    if missing_only:
+        missing = missing[missing['count'] > 0]
+
+    missing.sort_values(by=['count_pct'], inplace=True)
+    missing_train = np.array(missing['variables'])
+
+    if plot:
+        # Plot number of available data per variable
+        plt.subplots(figsize=(15, 6))
+
+        # Plots missing data in percentage
+        plt.subplot(1, 2, 1)
+        plt.barh(missing['variables'], missing['count_pct'])
+        plt.title('Count of missing training data in percent', fontsize=15)
+
+        # Plots total row number of missing data
+        plt.subplot(1, 2, 2)
+        plt.barh(missing['variables'], missing['count'])
+        plt.title('Count of missing training data as total records', fontsize=15)
+
+        plt.show()
+    return missing
